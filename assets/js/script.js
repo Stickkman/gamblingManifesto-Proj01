4b05b432869150d3331967543b3b50a21868b36b
pierr05 <pierreachlet322@gmail.com>
2022-12-20 18:47:57 -0500

    added code to script.js file

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+// variables go here 
+let team;
+let userInput = $('.user-input')
+let searchBtn = $('.search-Btn')
+let nfl = $('.football')
+// store api key inside quotation marks 
+let APIKey = " ";
+
+$(()=> {
+    // wrap all other code here
+
+}); 
